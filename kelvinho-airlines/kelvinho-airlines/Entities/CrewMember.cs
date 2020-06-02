c9fd6ed3e8d724d5663b49d9445c088d0f63f26c
KelvinRian <kelvinlourenco18@gmail.com>
2020-06-02 20:20:10 -0300

    defining which crew members cannot be together

--- kelvinho-airlines/kelvinho-airlines/Entities/CrewMember.cs
+++ kelvinho-airlines/kelvinho-airlines/Entities/CrewMember.cs
@@ -1,4 +1,5 @@
 ﻿using kelvinho_airlines.Enums;
+using System.Collections.Generic;
 
 namespace kelvinho_airlines.Entities
 {
@@ -6,11 +7,17 @@ namespace kelvinho_airlines.Entities
     {
         public string Name { get; protected set; }
         public CrewType CrewType { get; protected set; }
+        public List<string> CantBeAloneWith { get; protected set; } = new List<string>();
 
         public CrewMember(string name) : base()
         {
             Name = name;
+            SetCrewType();
+            SetCantBeAloneWith();
+        }
 
+        private void SetCrewType()
+        {
             if (this is Pilot || this is Officer)
             {
                 CrewType = CrewType.Technical;
@@ -22,6 +29,41 @@ namespace kelvinho_airlines.Entities
             else
             {
                 CrewType = CrewType.Common;
+            }
+        }
+
+        private void SetCantBeAloneWith()
+        {
+            if (!(this is Prisoner) && !(this is Policeman))
+            {
+                CantBeAloneWith.Add("Prisoner");
+            }
+            else if (this is Prisoner)
+            {
+                var crewMembers = new List<string>()
+                {
+                    "Officer",
+                    "FlightServiceChief",
+                    "Pilot",
+                    "FlightAttendant"
+                };
+                CantBeAloneWith.AddRange(crewMembers);
+            }
+            else if (this is Officer)
+            {
+                CantBeAloneWith.Add("FlightServiceChief");
+            }
+            else if (this is FlightServiceChief)
+            {
+                CantBeAloneWith.Add("Officer");
+            }
+            else if (this is Pilot)
+            {
+                CantBeAloneWith.Add("FlightAttendant");
+            }
+            else if (this is FlightAttendant)
+            {
+                CantBeAloneWith.Add("Pilot");
             }
         }
     }
